f1c22e96860c2a3d73f13eb2ae087e01eb394225
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2025-12-06 21:01:17 +0400

    Заменил объекты String в коллекции на Bus

--- src/BusList.java
+++ src/BusList.java
@@ -1,25 +1,24 @@
 import java.util.AbstractList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
-public class BusList extends AbstractList<String> {
+public class BusList extends AbstractList<Bus> {
 
     private final int INITIAL_CAPACITY = 10;
     private int capacity = INITIAL_CAPACITY;
     private int size = 0;
-    private String[] data = new String[INITIAL_CAPACITY];
+    private Bus[] data = new Bus[INITIAL_CAPACITY];
 
 
     public void resize() {
         capacity = (int) (capacity * 1.5);
-        String[] temp = new String[capacity];
+        Bus[] temp = new Bus[capacity];
         System.arraycopy(data, 0, temp, 0, data.length);
         data = temp;
     }
 
     @Override
-    public boolean add(String obj) {
+    public boolean add(Bus obj) {
         Objects.requireNonNull(obj, Messages.BUS_LIST_NULL_POINTER_EXCEPTION.getMessage());
         if (size + 1 > capacity) resize();
 
@@ -29,7 +28,7 @@ public class BusList extends AbstractList<String> {
     }
 
     @Override
-    public String get(int index) {
+    public Bus get(int index) {
         if (index < 0 || index >= size)
             throw new IndexOutOfBoundsException(Messages.BUS_LIST_INDEX_OUT_OF_BOUNDS_MESSAGE.getMessage());
         return data[index];
