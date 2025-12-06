457a834393026eb8e45406c5748da6f0fd70a35c
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2025-12-06 18:49:31 +0400

    Добавил метод add() и метод дин.расширения коллекции

--- src/BusList.java
+++ src/BusList.java
@@ -2,7 +2,27 @@ import java.util.AbstractList;
 
 public class BusList extends AbstractList<String> {
 
-    private String[] data = new String[10];
+    private final int INITIAL_CAPACITY = 10;
+    private int capacity = INITIAL_CAPACITY;
+    private int size = 0;
+    private String[] data = new String[INITIAL_CAPACITY];
+
+
+    public void resize() {
+        capacity = (int) (capacity * 1.5);
+        String[] temp = new String[capacity];
+        System.arraycopy(data, 0, temp, 0, data.length);
+        data = temp;
+    }
+
+    @Override
+    public boolean add(String s) {
+        if (size + 1 > capacity) resize();
+
+        data[size] = s;
+        size++;
+        return true;
+    }
 
     @Override
     public String get(int index) {
@@ -11,6 +31,6 @@ public class BusList extends AbstractList<String> {
 
     @Override
     public int size() {
-        return this.data.length;
+        return this.size;
     }
 }
