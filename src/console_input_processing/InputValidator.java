3c6fd30e147448f3a8d328d05b424d210bc1dc6f
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2025-12-09 19:04:23 +0400

    Добавил JavaDoc. Обработал варианты не корректного вызова getNumFromString()

--- src/console_input_processing/InputValidator.java
+++ src/console_input_processing/InputValidator.java
@@ -1,8 +1,20 @@
 package console_input_processing;
 
+/**
+ * Класс для проверки строкового инпута пользователя на соответствие определенным критерии.
+ * Все методы-валидаторы принимают String, возвращают true, если строка подходит, в противном случае false.
+ * <p>
+ * Метод getNumFromString возвращает сохраненное значение последнего успешного вызова Integer.pasrseInt(), и вызывать
+ * его надо следует ТОЛЬКО после успешной проверки на isInteger или isPositiveInteger. Это не самое элегантное решение
+ * сделано для того, чтобы не парсить одну и ту же строку несколько раз. А именно:
+ *          1)  в isInteger, для проверки число ли это
+ *          2)  в IsPositiveInteger, для проверки на положительность
+ *          3)  в самом InputProcessor, для возвращения результата
+ * Попытка вызвать метод getNumFromString при ложном isInteger или isPositiveInteger приведет к NullPointerException.
+ */
 public class InputValidator {
 
-    static private Integer numFromString;
+    static private Integer numFromString = null;
 
     public static boolean isNonEmptySting(String userInput) {
         return userInput != null && !userInput.isBlank();
@@ -15,15 +27,20 @@ public class InputValidator {
                 return true;
             } catch (NumberFormatException _) {}
         }
+        numFromString = null;
         return false;
     }
 
     public static boolean isPositiveInteger(String userInput) {
-        return (isInteger(userInput) && (numFromString >= 0));
+        if (isInteger(userInput) && (numFromString >= 0))
+            return true;
+        numFromString = null;
+        return false;
     }
 
     public static Integer getNumFromString() {
+        if (numFromString == null)
+            throw new NullPointerException("Попытка получить недавно обработанный валидный числовой инпут провалилась.");
         return numFromString;
     }
 }
-
