0da18e280a34ceee9306cba6eba8b8680087dbc1
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2025-12-11 20:13:40 +0400

    Рефактор класса InputValidator в StringValidator

---
+++ src/console_input_processing/StringValidator.java
@@ -0,0 +1,19 @@
+package console_input_processing;
+import java.util.regex.Pattern;
+
+/**
+ * Класс для проверки строк на соответствие определенным критериям.
+ * Все методы-валидаторы принимают String, возвращают true, если строка подходит, в противном случае false.
+ */
+public class StringValidator {
+
+    static Pattern intRegEx = Pattern.compile("^\\s*([+-]?[1-9]\\d*|0)\\s*$");
+
+    public static boolean isNonEmptySting(String userInput) {
+        return userInput != null && !userInput.isBlank();
+    }
+
+    public static boolean isInteger(String userInput) {
+        return intRegEx.matcher(userInput).matches();
+    }
+}
