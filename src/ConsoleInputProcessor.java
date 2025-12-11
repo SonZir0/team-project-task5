f8341ce1a255f2cd1c347e81705c41395dc9a7c8
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2025-12-12 00:09:51 +0400

    Добавил тесты классу ConsoleInputProcessor

--- src/ConsoleInputProcessor.java
+++ src/ConsoleInputProcessor.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Optional;
 
 /**
@@ -10,7 +7,7 @@ import java.util.Optional;
  * При не корректном инпуте со стороны пользователя, все методы должны выводить сообщение о типе ожидаемого инпута.
  * По завершению работы требуется вызвать close().
  */
-public class ConsoleInputProcessor {
+public class ConsoleInputProcessor implements Testable {
 
     private final BufferedReader bf;
 
@@ -68,6 +65,59 @@ public class ConsoleInputProcessor {
             bf.close();
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
+        }
+    }
+    @Override
+    public  void runAllTests() {
+        System.out.println("Запускаем тесты в классе ConsoleInputProcessor:");
+        System.out.println("\tТест метода получения непустой строки: " + ConsoleInputProcessor.Tests.testGetNonNullString());
+        System.out.println("\tТест метода получения целого числа: " + ConsoleInputProcessor.Tests.testTryToGetInteger());
+    }
+
+    static class Tests {
+        static boolean testGetNonNullString() {
+            // testData имеет строки оканчивающиеся на пробелы. Все что находится до /s является частью строки
+            String testData = """
+                    \t\t  \t
+                          \s
+                        \t    12  \s
+                    \t    \t    \t
+                    
+                    Success?""";
+            InputStream is = new ByteArrayInputStream(testData.getBytes());
+            ConsoleInputProcessor cip = new ConsoleInputProcessor(is);
+            // читаем первую "обрезанную" от нечитаемых символов непустую строку
+            return cip.getNonEmptyString().equals("12") &&
+                    cip.getNonEmptyString().equals("Success?");
+        }
+
+        /* Т.к. getInteger и getPositiveInteger выводят сообщения об ожидаемом инпуте в консоль (при тестах это
+        * не надо), то тестестировать будем метод, на который они полагаются - tryToGetInteger()     */
+        static boolean testTryToGetInteger() {
+            // testData имеет строки оканчивающиеся на пробелы. Все что находится до /s является частью строки
+            String testData = """
+                    \t\t  \t
+                    Test3
+                    \t\t+12\t\t
+                    -3-3
+                    ++9
+                    82-
+                    101.213
+                    Something
+                        \t    -20  \s
+                    Success?""";
+            InputStream is = new ByteArrayInputStream(testData.getBytes());
+            ConsoleInputProcessor cip = new ConsoleInputProcessor(is);
+            // Обращаю внимание, что все идущие подряд пустые строки будут "забраны" из потока за один вызов
+            return cip.tryToGetInteger().isEmpty() &&
+                    cip.tryToGetInteger().get() == 12 &&
+                    cip.tryToGetInteger().isEmpty() &&
+                    cip.tryToGetInteger().isEmpty() &&
+                    cip.tryToGetInteger().isEmpty() &&
+                    cip.tryToGetInteger().isEmpty() &&
+                    cip.tryToGetInteger().isEmpty() &&
+                    cip.tryToGetInteger().get() == -20 &&
+                    cip.tryToGetInteger().isEmpty();
         }
     }
 }
