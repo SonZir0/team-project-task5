cbb0adbf323061206481d951b4ffdef680bc8cae
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2025-12-13 20:17:58 +0400

    Рефактор Main. Добавил получение данных из инпута в основной цикл

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,47 @@
 public class Main {
 
+    static BusList busList;
+    static ConsoleInputProcessor inputProcessor;
+
     public static void main(String[] args) {
-        BusList busList = new BusList();
-        ConsoleInputProcessor inputProcessor = new ConsoleInputProcessor();
+        busList = new BusList();
+        inputProcessor = new ConsoleInputProcessor();
         Integer menuUserInput = 0;
 
         while (menuUserInput != -1) {
             System.out.println(Messages.MAIN_MENU_MESSAGE.getMessage());
             menuUserInput = inputProcessor.getInteger();
-
             switch (menuUserInput) {
                 case (1):
                     System.out.print("Размер коллекции: ");
                     busList.setSize( inputProcessor.getPositiveInteger() );
                     break;
                 case (2):
-                    while (menuUserInput != 0) {
-                        System.out.println(Messages.INPUT_STRATEGY_MENU_MESSAGE.getMessage());
-                        menuUserInput = inputProcessor.getInteger();
-                        switch (menuUserInput) {
-                            case (1):
-                                dummyFunction();
-                                break;
-                            case (2):
-                                dummyFunction();
-                                DataGetter dataGetter = new DataGetter(new GetDataFromFile());
-                                System.out.println("Результат работы getOneObject");
-                                dataGetter.getOneObject();
-                                System.out.println("Результат работы getNObjects");
-                                dataGetter.getNObjects(3);
-                                break;
-                            case (3):
-                                dummyFunction();
-                                break;
-                            case (0):
-                                break;
-                            default:
-                                System.out.println(Messages.DEFAULT_SWITCH_MESSAGE.getMessage());
-                                break;
-                        }
-                    }
+                    setDataStrategyMenu();
                     break;
                 case (3):
-                    MergeSort.mergeSort(busList);
+                    System.out.println(busList);
                     break;
                 case (4):
-                    SortEvenFieldOnly.sort(busList);
+                    if (busList.isEmpty())  System.out.println(Messages.COLLECTION_IS_EMPTY.getMessage());
+                    else {
+                        MergeSort.mergeSort(busList);
+                        System.out.println("Результат сортировки по 3-м полям:");
+                        System.out.println(busList);
+                    }
                     break;
                 case (5):
-                    dummyFunction();
+                    if (busList.isEmpty())  System.out.println(Messages.COLLECTION_IS_EMPTY.getMessage());
+                    else {
+                        SortEvenFieldOnly.sort(busList);
+                        System.out.println("Результат сортировки элементов с четными полям:");
+                        System.out.println(busList);
+                    }
                     break;
                 case (6):
+                    dummyFunction();
+                    break;
+                case (7):
                     dummyFunction();
                     break;
                 case (-1):
@@ -61,6 +52,53 @@ public class Main {
             }
         }
         inputProcessor.close();
+    }
+
+    private static void setDataStrategyMenu() {
+        while (true) {
+            System.out.println(Messages.INPUT_STRATEGY_MENU_MESSAGE.getMessage());
+            switch (inputProcessor.getInteger()) {
+                case (1):
+                    setDataMenu(new DataGetter(new GetDataFromInput(inputProcessor)));
+                    break;
+                case (2):
+                    dummyFunction();
+//                    dataGetter = new DataGetter(new GetDataFromFile());
+//                    System.out.println("Результат работы getOneObject");
+//                    dataGetter.getOneObject();
+//                    System.out.println("Результат работы getNObjects");
+//                    dataGetter.getNObjects(3);
+                    break;
+                case (3):
+                    dummyFunction();
+                    break;
+                case (0):
+                    return;
+                default:
+                    System.out.println(Messages.DEFAULT_SWITCH_MESSAGE.getMessage());
+                    break;
+            }
+        }
+    }
+
+    private static void setDataMenu(DataGetter dataGetter) {
+        while (true) {
+            System.out.println(Messages.SET_DATA_MENU_MESSAGE.getMessage());
+            switch (inputProcessor.getInteger()) {
+                case (1):
+                    busList.add(dataGetter.getOneObject());
+                    return;
+                case (2):
+                    System.out.print("Введите количество элементов(N): ");
+                    busList.addAll(dataGetter.getNObjects(inputProcessor.getPositiveInteger()));
+                    return;
+                case (0):
+                    return;
+                default:
+                    System.out.println(Messages.DEFAULT_SWITCH_MESSAGE.getMessage());
+                    break;
+            }
+        }
     }
 
     static void dummyFunction() {
