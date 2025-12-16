aaac940a926c8c625351dff58f12654c68baebb1
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2025-12-16 16:43:42 +0400

    Объединил 2 и 3 тесты в GetDataFromInput, добавил вызов теста класса в main

--- src/GetDataFromInput.java
+++ src/GetDataFromInput.java
@@ -53,8 +53,6 @@ public class GetDataFromInput implements GetData, Testable {
         System.out.println("Запускаем тесты в классе GetDataFromInput:");
         System.out.println("\tТест получения одного объекта: " + GetDataFromInput.Tests.testOutputObject());
         System.out.println("\tТест получения листа из N элементов: " + GetDataFromInput.Tests.testOutputList());
-        System.out.println("\tТест получения пустого листа при N = 0: " + GetDataFromInput.Tests.testNIsZero());
-
     }
 
     static class Tests {
@@ -97,25 +95,13 @@ public class GetDataFromInput implements GetData, Testable {
             try (ConsoleInputProcessor cip = new ConsoleInputProcessor(is)) {
                 GetDataFromInput dataFromInput = new GetDataFromInput(cip);
                 BusList temp = dataFromInput.getNObjects(4, true).get();
+                BusList temp2 = dataFromInput.getNObjects(0, true).get();
                 // все идущие подряд пустые строки будут пропущены за один вызов
                 return temp.size() == 4 &&
                         temp.get(1).getNumber().equals("Mileage?") &&
                         temp.get(2).getModel().equals("00000") &&
-                        temp.getLast().getMileage() == 51;
-            }
-        }
-
-        static boolean testNIsZero() {
-            String testData = """
-                    517K
-                    \t    RF-102-\t
-                    +1000
-                    Success?""";
-            InputStream is = new ByteArrayInputStream(testData.getBytes());
-            try (ConsoleInputProcessor cip = new ConsoleInputProcessor(is)) {
-                GetDataFromInput dataFromInput = new GetDataFromInput(cip);
-                BusList temp = dataFromInput.getNObjects(0, true).get();
-                return temp.isEmpty();
+                        temp.getLast().getMileage() == 51 &&
+                        temp2.isEmpty();
             }
         }
     }
